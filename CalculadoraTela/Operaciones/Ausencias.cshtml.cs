7ea03d3ff6bb74085a6baa45a95858a5d6e0a8e3
Antonio Vidaurre <josevidaurrealejo@gmail.com>
2026-07-22 19:37:50 -0400

    Agregando vistas Ausencias y Rendimiento en Operaciones

---
+++ CalculadoraTela/Operaciones/Ausencias.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CalculadoraTela.Operaciones
+{
+    public class AusenciasModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ CalculadoraTela/Operaciones/Rendimiento.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CalculadoraTela.Operaciones
+{
+    public class RendimientoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
