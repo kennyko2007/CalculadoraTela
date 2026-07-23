2b7261ce77887ac9e2a17f20a14ef55794b76386
kennyko2007 <josevidaurrealejo@gmail.com>
2026-07-22 22:53:51 -0400

    Update CalculadoraService.cs

--- CalculadoraTela/Services/CalculadoraService.cs
+++ CalculadoraTela/Services/CalculadoraService.cs
@@ -1,4 +1,4 @@
-﻿using CalculadoraTela.ViewModels;
+using CalculadoraTela.ViewModels;
 
 namespace CalculadoraTela.Services
 {
@@ -8,22 +8,22 @@ namespace CalculadoraTela.Services
         {
             if (model == null) return new CalculadoraTelaVM();
 
-            // 1. Factor según el Tipo de Producto
+            // 1. Factor según el Tipo de Producto (Plana = 1.0, Tubular = 2.0)
             decimal factorProducto = model.TipoProducto == "Tubular" ? 2.0m : 1.0m;
 
             // 2. Cálculo de Resistencia y Peso de Urdimbre
-            model.ResistenciaUrdimbre = (model.UrdimbreDenier > 0m && model.CintaUrdimbre > 0m) 
-                ? (model.UrdimbreTejido * model.UrdimbreDenier * model.CintaUrdimbre) / 1000.0m : 0m;
+            model.ResistenciaUrdimbre = (model.UrdimbreTejido > 0m && model.UrdimbreDenier > 0m) 
+                ? (model.UrdimbreTejido * model.UrdimbreDenier * 0.0471m) : 0m;
             
             model.PesoUrdimbre = (model.UrdimbreTejido > 0m && model.UrdimbreDenier > 0m) 
-                ? (model.UrdimbreTejido * model.UrdimbreDenier) / 9000.0m * factorProducto : 0m;
+                ? (model.UrdimbreTejido * model.UrdimbreDenier) / 450.0m : 0m;
 
             // 3. Cálculo de Resistencia y Peso de Trama
-            model.ResistenciaTrama = (model.TramaDenier > 0m && model.CintaTrama > 0m) 
-                ? (model.TramaTejido * model.TramaDenier * model.CintaTrama) / 1000.0m : 0m;
+            model.ResistenciaTrama = (model.TramaTejido > 0m && model.TramaDenier > 0m) 
+                ? (model.TramaTejido * model.TramaDenier * 0.0471m) : 0m;
 
             model.PesoTrama = (model.TramaTejido > 0m && model.TramaDenier > 0m) 
-                ? (model.TramaTejido * model.TramaDenier) / 9000.0m * factorProducto : 0m;
+                ? (model.TramaTejido * model.TramaDenier) / 450.0m : 0m;
 
             // 4. Porcentajes de participación
             decimal totalPesoTejido = model.PesoUrdimbre + model.PesoTrama;
@@ -39,8 +39,7 @@ namespace CalculadoraTela.Services
             }
 
             // 5. Urdimbre Refuerzo / Resistencia de Refuerzo
-            model.UrdimbreRefuerzoResistencia = (model.DenierRefuerzo > 0m && model.CintaRefuerzo > 0m) 
-                ? (model.UrdimbreRefuerzoTejido * model.DenierRefuerzo * model.CintaRefuerzo) / 1000.0m : 0m;
+            model.UrdimbreRefuerzoResistencia = model.ResistenciaUrdimbre;
 
             // 6. Peso Tejido Base (GM2)
             model.PesoTejidoBase = totalPesoTejido;
@@ -48,20 +47,22 @@ namespace CalculadoraTela.Services
             // 7. Peso con Laminado (GM2 PP+LAM)
             model.PesoConLaminado = model.PesoTejidoBase + model.Laminado;
 
-            // 8. GMP (Peso con Refuerzo)
-            decimal factorRefuerzo = 1.0m + (model.AnchoRefuerzoFactor * 0.01m);
-            model.PesoConRefuerzo = factorRefuerzo * model.PesoConLaminado;
+            // 8. GMP (Peso con Refuerzo sumando directamente el factor de ancho de refuerzo del 0 al 12)
+            model.PesoConRefuerzo = model.PesoConLaminado + model.AnchoRefuerzoFactor;
 
             // 9. GML (Gramos por Metro Lineal)
-            model.PesoMetroLineal = factorProducto * model.PesoConRefuerzo * (model.Ancho / 100.0m);
+            model.PesoMetroLineal = (model.PesoConRefuerzo * model.Ancho * 2.0m) / 1000.0m;
 
-            // 10. Cantidad de Conos (REDONDEA.PAR)
-            if (model.UrdimbreTejido > 0m)
+            // 10. Cantidad de Conos (Equivalente exacto a REDONDEA.PAR de Excel)
+            if (model.UrdimbreTejido > 0m && model.Ancho > 0m)
             {
                 decimal calculoConos = (model.Ancho * (factorProducto * 10.0m)) / (model.UrdimbreTejido / 1.035m);
-                int conosRedondeados = (int)Math.Ceiling((double)calculoConos);
-                if (conosRedondeados % 2 != 0) conosRedondeados += 1;
+                int conosRedondeados = (int)Math.Ceiling(calculoConos / 2.0m) * 2;
                 model.MaquinaNumero = conosRedondeados;
+            }
+            else
+            {
+                model.MaquinaNumero = 0;
             }
 
             return model;
