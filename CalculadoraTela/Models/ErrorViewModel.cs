c1efe38ff33dd11b1e68d56cade063531cc5b3e1
kennyko2007 <josevidaurrealejo@gmail.com>
2026-07-24 11:45:59 -0400

    Add files via upload

---
+++ CalculadoraTela/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CalculadoraTela.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
